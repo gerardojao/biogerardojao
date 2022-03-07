e3c602334bf0eec7de807866bf5e423bb93eeed7
gerardojao <gerardojao@gmail.com>
2022-03-07 10:38:02 -0400

    EDIT DATA

--- src/Data.js
+++ src/Data.js
@@ -3,26 +3,20 @@ export default [
     {
         id:1,
         link:"https://gerardojao.github.io/gerardojao/#portfolio",
-        name:"Portfolio",
+        name:"Portfolio"
       
     },
     {
         id:2,
         link:"https://gerardojao.github.io/My-Blog/",
-        name:"Personal Blog",
+        name:"Personal Blog"
     
     },
     {
         id:3,
         link:"https://www.youtube.com/watch?v=i-qtA-DvmcU&feature=youtu.be&ab_channel=Gerardojao",
-        name:"Youtube Channel",
+        name:"Youtube Channel"
         
     }
-    // ,
-    // {
-    //     id:4,
-    //     link:"/biogerardojao/static/media/CV Gerardo Arrieta.106fab5835521328ae58.pdf",
-    //     name:"Download CV",
-    //     download:"Gerardo Arrieta CV"
-    // }
+ 
 ]
