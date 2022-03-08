dbaeacb4d9a5143ac829297489df800032e465f5
gerardojao <gerardojao@gmail.com>
2022-03-07 23:28:49 -0400

    responsive horizontal

--- src/Components/Network.js
+++ src/Components/Network.js
@@ -5,10 +5,10 @@ const Network = () => {
   return (
   
     <footer className="footer">
-    <a className="button-download" href={CV} target="_blank">Download CV</a>
+    <a className="button-download" href={CV} target="_blank"><i className="hiddenIcon fas fa-download"></i><span className="hiddenDownload">Download </span> CV</a>
     <br />
     <br />
-	<div className="rrss">
+  	<div className="rrss">
 		<a title="LinkedIn" href="https://linkedin.com/in/gerardojao" target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in" aria-hidden="true"></i></a>
 		<a title="GitHub" href="https://github.com/gerardojao" target="_blank" rel="noreferrer"><i className="fab fa-github-alt" aria-hidden="true"></i></a>
 		<a title="Twitter" href="https://twitter.com/gerardojao" target="_blank" rel="noreferrer"><i className="fab fa-twitter" aria-hidden="true"></i></a>
