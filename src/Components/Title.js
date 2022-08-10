77ce42077256a1b9f7cc1711fbc25c2ee1f5d0ae
gerardojao <gerardojao@gmail.com>
2022-08-09 21:19:43 -0400

    add logo

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,11 +1,14 @@
 import Typed from 'react-typed';
+import img from "../assets/logo.svg"
 
 const Title = () => {
   return (
-    <div >
-
+    <div className="containerTitle" >
+      <div className="logoTitle">
+        <a href="https://gerardojao.vercel.app/" target="_blank"> <img className="logo" src={img} alt="log"  /></a>
+   
     <div className="home__content--1"><span>G</span>erardo <span>A</span>rrieta</div>
-  
+    </div>
     <Typed
                 strings={[
                    " Frontend Developer", "Backend Developer"]}
