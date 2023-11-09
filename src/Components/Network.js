93f1cb8f5c00f202f39f2aae5fec895d8242a069
gerardo arrieta <gerardojao@gmail.com>
2023-11-09 17:33:51 +0100

    add ebooks

--- src/Components/Network.js
+++ src/Components/Network.js
@@ -5,8 +5,8 @@ const Network = () => {
   return (
   
     <footer className="footer">
-    <a className="button-download" href={CV} target="_blank"><i className="hiddenIcon fas fa-download"></i><span className="hiddenDownload">Download </span> CV</a>
-    <br />
+    {/* <a className="button-download" href={CV} target="_blank"><i className="hiddenIcon fas fa-download"></i><span className="hiddenDownload">Download </span> CV</a>
+    <br /> */}
     <br />
   	<div className="rrss">
 		<a title="LinkedIn" href="https://linkedin.com/in/gerardojao" target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in" aria-hidden="true"></i></a>
--- src/Data.js
+++ src/Data.js
@@ -1,11 +1,11 @@
 
 export default [
-    {
-        id:1,
-        link:"https://gerardojao.vercel.app/",
-        name:"Portfolio"
+    // {
+    //     id:1,
+    //     link:"https://gerardojao.vercel.app/",
+    //     name:"Portfolio"
       
-    },
+    // },
     // {
     //     id:2,
     //     link:"https://gerardojao.github.io/My-Blog/",
@@ -21,12 +21,22 @@ export default [
     {
         id:4,
         link:"https://axelfas.github.io/ebook/",
-        name:"Ebook Programar a los 40's v1.0"  
+        name:"Ebook Programar a los 40's"  
     },
     {
         id:5,
         link:"https://t.co/JCTXbRn3b7",
-        name:"Ebook Programar a los 40's v2.0"  
+        name:"Mi Experiencia como programador luego de los 40's"  
+    },
+    {
+        id:6,
+        link:"https://t.co/JCTXbRn3b7",
+        name:"Las Soft Skills que todo programador debe tener"  
+    },
+    {
+        id:7,
+        link:"https://www.youtube.com/watch?v=8fdcosQhHNM&t=4s",
+        name:"Charla MI primer año como desarrollador con 45 años"  
     }
  
 ]
